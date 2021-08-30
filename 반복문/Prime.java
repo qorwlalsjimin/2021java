d3b2ec88f15b66cafc96946b06dbb1ba65331ece
Baek Jimin <s2104@e-mirim.hs.kr>
2021-08-30 20:53:59 +0900

    DoWhile을 이용한 소수 판단하기

--- 반복문/Prime.java
+++ 반복문/Prime.java
@@ -1,33 +1,19 @@
 import java.util.Scanner;
-public class Prime {
+public class Prime{
 	public static void main( String args[]){
 		Scanner scan = new Scanner(System.in);
-
-		System.out.print("숫자 입력 : ");
+		System.out.print("숫자를 입력하시오. >> ");
 		int n = scan.nextInt();
-		int iresult=0;
-
-	
-		for( int i=1; i<=n; i++) {
-			if (n%i==0){
-				iresult += 1;
-			}	
+		int cnt = 0;
+		int i = 1;
+		do{
+			if(n%i==0) 
+				cnt++;
+			i++;
 		}
+		while(i<=n);
 
-		if (iresult == 2)
-			System.out.println(n+"은 소수이다.");
-		else
-			System.out.println(n+"은 소수가 아니다.");
-
+		if(cnt==2) System.out.println("소수이다.");
+		else System.out.println("소수가 아니다.");
 	}
 }
-
-/*
-소수 : 1과 자신만을 약수로 가지는 수. 약수가 2개임. 3개이상 ㄴㄴ
-약수가 나올 때마다 카운팅
-2/1 2/2
-3/1 (3/2)  3/3
-4/1 4/2   (4/3) 4/4
-5/1 (5/2) (5/3) (5/4) 5/5
-6/1 6/2   6/3  (6/4) 6/5
-*/
