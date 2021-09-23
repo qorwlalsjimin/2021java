3e3951dbc663fef5c847017e750a0d1b1b96ca3c
Baek Jimin <s2104@e-mirim.hs.kr>
2021-09-23 19:30:38 +0900

    연속된 짝수 3개를 곱했을 때 결과값이 4****2인 숫자 3개 구하기

---
+++ 반복문/Quiz_01.java
@@ -0,0 +1,20 @@
+package Study;
+
+public class Quiz_01 {
+
+	public static void main(String[] args) {
+		
+		// 100미만의 연속된 두 자릿수의 짝수 3개를 곱하였더니, 4****2가 되었다.
+		// 이 세 개의 수는 어떤 수인지, 그리고 그 결과는 얼마인지 구하는 프로그램을
+		// for문을 이용해서 작성하시오.
+		int cnt=1;
+		for(int i = 2; i<100; i+=2) {
+			for(int j = 1; j<=3; j++)
+				cnt = i*(i+2)*(i+4);
+			if(cnt/100000==4 && cnt%10==2)
+				System.out.println(i+" "+(i+2)+" "+(i+4));
+		}
+
+	}
+
+}
