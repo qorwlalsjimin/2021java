f80f480e1b3354c83f10032f4b0ebf589591a410
Baek Jimin <s2104@e-mirim.hs.kr>
2021-10-01 10:20:47 +0900

    삼각형으로 숫자 찍기

---
+++ 배열/ArrayExam13.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class ArrayExam13 {
+
+	public static void main(String[] args) {
+		
+		int k = 1;
+		int[][] one = new int[4][4];
+		for(int i = 0; i<one.length; i++) {
+			for(int j = 0; j<=i; j++) {
+				one[i][j] = k++;
+			}
+
+		}
+		
+		System.out.println("-------------자바실행-------------");
+		for(int i = 0; i<one.length; i++) {
+			for(int j = 0; j<one[i].length; j++) {
+				if(one[i][j]==0) continue;
+				System.out.print(one[i][j]+"\t");
+			}
+			System.out.println();
+		}//for
+
+
+	}
+
+}
