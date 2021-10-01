5fc40fe9d78c57fa3629667a058d0c17fbc14b87
Baek Jimin <s2104@e-mirim.hs.kr>
2021-10-01 10:07:21 +0900

    2차원 배열 입출력 연습예제 응용

---
+++ 배열/ArrayExam12.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class ArrayExam12 {
+
+	public static void main(String[] args) {
+
+		int k = 1;
+		int[][] one = new int[4][4];
+		for(int i = 0; i<one.length; i++) {
+			for(int j = 0; j<one[i].length; j++) {
+				one[i][j] = k++;
+			}
+
+		}
+		
+		System.out.println("-------------자바실행-------------");
+		for(int i = 0; i<one.length; i++) {
+			for(int j = 0; j<one[i].length; j++)
+				System.out.print(one[i][j]+"\t");
+			System.out.println();
+		}//for
+
+	}
+
+}
