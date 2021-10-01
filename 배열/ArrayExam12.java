9360a0356f95647d0d8872294cb56f412a6939b1
Baek Jimin <s2104@e-mirim.hs.kr>
2021-10-01 10:09:47 +0900

    변수 하나 덜 쓰는 코드

--- 배열/ArrayExam12.java
+++ 배열/ArrayExam12.java
@@ -4,11 +4,12 @@ public class ArrayExam12 {
 
 	public static void main(String[] args) {
 
-		int k = 1;
+		//int k = 1;
 		int[][] one = new int[4][4];
 		for(int i = 0; i<one.length; i++) {
 			for(int j = 0; j<one[i].length; j++) {
-				one[i][j] = k++;
+				//one[i][j] = k++;
+				one[i][j] = i*4+(j+1);
 			}
 
 		}
