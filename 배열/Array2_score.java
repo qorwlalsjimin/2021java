bdceda95e68bf596b10e2e46ae066d2b245437ad
Baek Jimin <s2104@e-mirim.hs.kr>
2021-10-15 08:40:34 +0900

    0~20 사이의 수 조건 추가

--- 배열/Array2_score.java
+++ 배열/Array2_score.java
@@ -6,25 +6,23 @@ public class Array2_score {
 		
 		Scanner scan = new Scanner(System.in);
 		int[][] score = new int[5][3];
-		int max=0, team=0, space = 0;
+		int max=0, team=0;
 		
 		for(int i = 0; i<score.length; i++) {
+			int space = 0;
 			System.out.print(i+1+"조 >> ");
-			
 			for(int j = 0; j<score[i].length; j++) {
 				score[i][j] = scan.nextInt();
-				if(score[i][j]>20 || score[i][j]<0) {
+				while(score[i][j]>20 || score[i][j]<0) {
 					System.out.println("다시 입력하시오.");
 					score[i][j] = scan.nextInt();
 				}//if
 				space += score[i][j];
 			}//for
-			
 			if(max<space) {
 				max = space;
 				team = i+1;
 			}//if
-			space = 0;
 		}//for
 		System.out.println("최고팀 : "+team+"번째 팀 -> 점수 : "+max);
 	}
