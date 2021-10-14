7b685a86c3dcabd7b09f0bd302436e1fe0d9fe36
Baek Jimin <s2104@e-mirim.hs.kr>
2021-10-15 08:57:26 +0900

    점수 종류 추가

--- 배열/Array2_score.java
+++ 배열/Array2_score.java
@@ -6,15 +6,19 @@ public class Array2_score {
 		
 		Scanner scan = new Scanner(System.in);
 		int[][] score = new int[5][3];
+		String[] sub_title = new String[]{"완성도", "창의성", "이해도"};
+
 		int max=0, team=0;
 		
 		for(int i = 0; i<score.length; i++) {
 			int space = 0;
-			System.out.print(i+1+"조 >> ");
+			System.out.println("*"+(i+1)+"조 입력");
 			for(int j = 0; j<score[i].length; j++) {
+				System.out.print(sub_title[j]+" : ");
 				score[i][j] = scan.nextInt();
 				while(score[i][j]>20 || score[i][j]<0) {
 					System.out.println("다시 입력하시오.");
+					System.out.print(sub_title[j]+" : ");
 					score[i][j] = scan.nextInt();
 				}//if
 				space += score[i][j];
@@ -24,7 +28,7 @@ public class Array2_score {
 				team = i+1;
 			}//if
 		}//for
-		System.out.println("최고팀 : "+team+"번째 팀 -> 점수 : "+max);
+		System.out.println("\n최고팀 : "+team+"번째 팀 -> 점수 : "+max);
 	}
 
 }
