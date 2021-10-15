31e7bc846c5b65e2d0d7b1e9b6c6e923292c1055
Baek Jimin <s2104@e-mirim.hs.kr>
2021-10-15 09:45:07 +0900

    합계 추가 & 각 팀별 종합 점수 배열에 넣기

--- 배열/Array2_score.java
+++ 배열/Array2_score.java
@@ -6,39 +6,49 @@ public class Array2_score {
 		
 		Scanner scan = new Scanner(System.in);
 		int[][] score = new int[5][3];
-		String[] sub_title = new String[]{"완성도", "창의성", "이해도"};
+		int[] sum = new int[5];
+		String[] sub_title = new String[]{"완성도", "창의성", "이해도"," 합계"};
 
 		int max=0, team=0;
 		
 		for(int i = 0; i<score.length; i++) {
-			int space = 0;
 			System.out.println("*"+(i+1)+"조 입력");
+			sum[i] = 0;
 			for(int j = 0; j<score[i].length; j++) {
 				System.out.print(sub_title[j]+" : ");
 				score[i][j] = scan.nextInt();
-//				if(score[i][j]>20 || score[i][j]<0)
+//				if(score[i][j]>20 || score[i][j]<0) {
 //					j--;
+//					space = 0;
+//				}
 				while(score[i][j]>20 || score[i][j]<0) {
 					System.out.println("다시 입력하시오.");
 					System.out.print(sub_title[j]+" : ");
 					score[i][j] = scan.nextInt();
 				}//while
-				space += score[i][j];
+				sum[i] += score[i][j];
 			}//for
-			if(max<space) {
-				max = space;
+			if(max<sum[i]) {
+				max = sum[i];
 				team = i+1;
 			}//if
 		}//for
 		
+		//출력
+		System.out.println("=========================================");
+		for(int k = 0; k<sub_title.length; k++)
+			System.out.print("\t"+sub_title[k]);
+		System.out.print("\n=========================================");
 		for(int i = 0; i<score.length; i++) {
 			System.out.println();
-			System.out.print(i+1+"조 : ");
+			System.out.print(i+1+"조 : \t");
 			for(int j = 0; j<score[i].length; j++) {
-				System.out.print(score[i][j]+" ");
+				System.out.print(score[i][j]+"\t");
 			}
+			System.out.println(sum[i]);
 		}
-		System.out.println("\n\n최고팀 : "+team+"번째 팀 -> 점수 : "+max);
+		System.out.println("\n=========================================");
+		System.out.println("\n최고팀 : "+team+"번째 팀 -> 점수 : "+max);
 	}
 
 }
