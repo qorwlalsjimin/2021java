f8f65b49c9de02786e0a438d031d938b2d6ec2f4
Baek Jimin <s2104@e-mirim.hs.kr>
2021-10-21 21:24:00 +0900

    Call by reference로 입력 받은 5자리 숫자의 각 자리 숫자들의 합

---
+++ 반복문/Test_05.java
@@ -0,0 +1,22 @@
+package Study;
+
+public class Test_05 {
+//	Test_05 tt = new Test_05();
+//	tt.main(args);
+	public static void main(String[] args) {
+
+//		double d = Math.random();
+//		System.out.println(d);
+//		System.out.println(Math.random());
+
+//		for(int i = 1; i<=10; i++)
+//			System.out.println(Math.random());
+		
+		for(int i = 1; i<=100; i++) {
+			int num = (int)(Math.random()*100)+1; //1~100³ª¿È
+			System.out.println(num);
+		}
+
+	}
+
+}
