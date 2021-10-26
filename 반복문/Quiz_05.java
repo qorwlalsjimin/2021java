e666f58461541f48568600198c39b28824ef8c79
Baek Jimin <s2104@e-mirim.hs.kr>
2021-10-26 18:57:01 +0900

    더 간단한 코드

--- 반복문/Quiz_05.java
+++ 반복문/Quiz_05.java
@@ -7,19 +7,27 @@ public class Quiz_05 {
 	public static void main(String[] args) {
 
 		//for문을 이용해서 문자형 숫자를 입력받아서 각 자리의 합을 구하시오.
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
 		
-		Scanner scan = new Scanner(System.in);
-		String n = scan.next();
-		char nn;
-		int length = n.length();
-		
-		int tot = 0;
-		for(int i = 0; i<length; i++) {
-			nn = n.charAt(i);
-			tot += nn-'1'+1;
+		for(int i = 0; i<str.length(); i++) {
+			sum += str.charAt(i)-'0';
 		}
-		System.out.println(tot);
+		System.out.println(sum);
+		
+//		Scanner scan = new Scanner(System.in);
+//		String n = scan.next();
+//		char nn;
+//		int length = n.length();
+//		
+//		int tot = 0;
+//		for(int i = 0; i<length; i++) {
+//			nn = n.charAt(i);
+//			tot += nn-'1'+1;
+//		}
+//		System.out.println(tot);
 
-	}
+	} //main
 
 }
