7154fc9ae5ee786fd70818b1554467b882feea83
Baek Jimin <s2104@e-mirim.hs.kr>
2021-11-12 09:58:10 +0900

    좌표 상의 점을 모델링하여 점을 생성하고 이동하는 프로그램을 작성해보자

---
+++ 클래스와 객체/MakePoint.java
@@ -0,0 +1,44 @@
+package Class;
+
+import java.util.Scanner;
+
+class dot{
+
+	int x;
+	int y;
+	
+	void move(int xm, int ym) {
+		x += xm;
+		y += ym;
+	}
+	
+	void print() {
+		System.out.println("("+x+", "+y+")");
+	}
+}
+
+public class MakePoint {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("x,y 좌표를 입력하시오 >> ");
+		
+		dot one = new dot();
+		one.x = scan.nextInt();
+		one.y = scan.nextInt();
+		one.print();
+		
+		System.out.print("이동할 값을 x,y 각각 입력하시오 >> ");
+		int move_x = scan.nextInt();
+		int move_y = scan.nextInt();
+		
+		one.move(move_x,move_y);
+		one.print();
+		
+		
+		
+
+	}
+
+}
